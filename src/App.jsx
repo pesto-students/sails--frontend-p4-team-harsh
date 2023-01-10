5f7a958e410162461d48e7fd1090f0d7d073f184
kunal sehji <kunal.sehji@techchefz.com>
2023-01-11 02:49:37 +0530

    Added routing + Sidebar UI fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import "./styles/app.scss";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import AdminDashboard from "./screens/adminDashboard";
+import LoginPage from "./screens/loginPage";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <AdminDashboard />
+        <Routes>
+          <Route path="/" element={<AdminDashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </Layout>
     </div>
   );
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,44 +1,49 @@
-import React from 'react';
-// import '../App.css';
-import { Sidebarlist } from './Sidebarlist';
-import { Setting } from './Sidebarlist';
-import styles from './index.module.scss';
+import React from "react";
+import { Sidebarlist } from "./Sidebarlist";
+import { Setting } from "./Sidebarlist";
+import styles from "./index.module.scss";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
-    return (
-        <div className={styles.sideBar}>
-            <div className={styles.sails}>Sails</div>
-            <ul className={styles.Sidebardata}>
-                {Sidebarlist.map((val, key) => {
-                    return (
-                        <li key={key}
-                            className={styles.row}
-                            id={window.location.pathname === val.link ? "active" : " "}
-                            onClick={() => { window.location.pathname = val.link; }}>
-                            <div id='icon'>{val.icon}</div>
-                            <div id='title'>{val.title}</div>
-                        </li>
-                    );
-                })}
-            </ul>
+  return (
+    <div
+      className={styles.sideBar}
+      style={{ display: window.location.pathname === "/login" ? "none" : "" }}
+    >
+      <div className={styles.sails}>Sails</div>
+      <ul className={styles.Sidebardata}>
+        {Sidebarlist.map((val, key) => {
+          return (
+            <Link to={val.link} key={key}>
+              <li
+                className={styles.row}
+                id={window.location.pathname === val.link ? "active" : " "}
+              >
+                <div id="icon">{val.icon}</div>
+                <div id="title">{val.title}</div>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
 
-
-            <ul className={styles.Setting}>
-                {Setting.map((val, key) => {
-                    return (
-                        <li key={key}
-                            className={styles["row-s"]}
-                            id={window.location.pathname === val.link ? "active" : " "}
-                            onClick={() => { window.location.pathname = val.link; }}>
-                            <div id='icon-s'>{val.icon}</div>
-                            <div id='title-s'>{val.title}</div>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-
-    )
+      <ul className={styles.Setting}>
+        {Setting.map((val, key) => {
+          return (
+            <Link to={val.link} key={key}>
+              <li
+                className={styles["row-s"]}
+                id={window.location.pathname === val.link ? "active" : " "}
+              >
+                <div id="icon-s">{val.icon}</div>
+                <div id="title-s">{val.title}</div>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
