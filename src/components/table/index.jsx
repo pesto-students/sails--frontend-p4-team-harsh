fd66d9d3728d9176d2f2dc1a6af85760945f0c29
kunal sehji <kunal.sehji@techchefz.com>
2023-01-19 20:00:06 +0530

    Resolved console warnings

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -4,26 +4,30 @@ import styles from "./index.module.scss";
 const Index = ({ leadsData }) => {
   return (
     <table className={styles.container}>
-      <tr>
-        {Object.keys(leadsData.data[0]).map((item) => (
-          <th>{item}</th>
-        ))}
-        <th>CTA</th>
-        <th>Reschedule</th>
-      </tr>
-      {leadsData.data.map((item) => (
+      <thead>
         <tr>
-          {Object.values(item).map((value) => (
-            <td>{value}</td>
+          {Object.keys(leadsData.data[0]).map((item, index) => (
+            <th key={`lead-table-head-${index}`}>{item}</th>
           ))}
-          <td>
-            <button>Send Email</button>
-          </td>
-          <td>
-            <input type="date" />
-          </td>
+          <th>CTA</th>
+          <th>Reschedule</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {leadsData.data.map((item, index) => (
+          <tr key={`leads-row-${index}`}>
+            {Object.values(item).map((value, index) => (
+              <td key={`leads-row-data-${index}`}>{value}</td>
+            ))}
+            <td>
+              <button>Send Email</button>
+            </td>
+            <td>
+              <input type="date" />
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
