c03236a5f665c41b552e26e45e1d7171dcd3f785
kunal sehji <kunal.sehji@techchefz.com>
2023-01-22 00:25:58 +0530

    Added Add Company screen

--- src/components/addCompanyForm/index.jsx
+++ src/components/addCompanyForm/index.jsx
@@ -5,22 +5,32 @@ const Index = ({ onSubmit, data, handleChange }) => {
   return (
     <div className={styles.container}>
       <form onSubmit={onSubmit}>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter Name"
-            name="name"
-            value={data.name}
-            onChange={(e) => handleChange(e.target.name, e.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="User Count"
-            name="userCount"
-            value={data.userCount}
-            onChange={(e) => handleChange(e.target.name, e.target.value)}
-          />
+        <div className={styles.form__row}>
+          <div className={styles.form__group}>
+            <label htmlFor="register-company-name">Name</label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              name="name"
+              id="register-company-name"
+              value={data.name}
+              onChange={(e) => handleChange(e.target.name, e.target.value)}
+            />
+          </div>
+          <div className={styles.form__group}>
+            <label htmlFor="register-company-count">User Count</label>
+            <input
+              type="number"
+              placeholder="Enter User Count"
+              name="userCount"
+              id="register-company-count"
+              value={data.userCount}
+              onChange={(e) => handleChange(e.target.name, e.target.value)}
+            />
+          </div>
         </div>
+        <div>API Key: {data.apiKey}</div>
+        <div>Valid: {data.validity ? "True" : "False"}</div>
         <button className="button-primary">Submit</button>
       </form>
     </div>
--- src/screens/addCompany/index.jsx
+++ src/screens/addCompany/index.jsx
@@ -5,7 +5,7 @@ import styles from "./index.module.scss";
 
 const initState = {
   name: "",
-  apiKey: "",
+  apiKey: "ADSLKFJOWAEJFLAKJSDKLFDJF234243234",
   validity: true,
   userCount: 0,
 };
